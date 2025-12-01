5913f8c24cc4886a0831022b042a8a27470eed32
ssoosy <soy1006@khu.ac.kr>
2025-12-01 14:45:15 +0900

    sweat_detail UI

--- lib/features/main.dart
+++ lib/features/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'search/kimchi_detail.dart';
+import 'search/pot_detail.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Cleaning Info',
-          home: const KimchiDetail(),
+          home: const PotDetail(),
         );
       },
     );
