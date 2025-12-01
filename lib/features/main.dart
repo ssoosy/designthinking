451841504379cb99628ec2cb134e3b1e7163bcfd
ssoosy <soy1006@khu.ac.kr>
2025-12-01 12:37:54 +0900

    laundry_category UI

--- lib/features/main.dart
+++ lib/features/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'search/smell_category.dart';
+import 'search/laundry_category.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Cleaning Info',
-          home: const SmellCategory(),
+          home: const LaundryCategory(),
         );
       },
     );
