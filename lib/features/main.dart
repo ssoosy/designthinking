0818f960cefab47b2f4c18503446982a82bbe383
ssoosy <soy1006@khu.ac.kr>
2025-12-01 11:43:02 +0900

    remove_category UI

--- lib/features/main.dart
+++ lib/features/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'search/search_info.dart';
+import 'search/remove_category.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Cleaning Info',
-          home: const SearchInfo(),
+          home: const RemoveCategory(),
         );
       },
     );
